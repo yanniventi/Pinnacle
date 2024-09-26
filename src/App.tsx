22ae818116da3bec0e5835bc822069725e233c10
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2024-09-26 13:55:30 +0800

    Config update
    Update eslint config for tailwind. Added tailwind prettier plugin

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="h-screen w-full flex justify-center items-center flex-col">
-      <h1>Vite + React</h1>
-      <Button variant={"destructive"} onClick={() => setCount((count) => count + 1)}>
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+      <h1 className="text-lg font-semibold">Vite + React</h1>
+      <Button
+        variant={"destructive"}
+        onClick={() => setCount((count) => count + 1)}
+      >
         count is {count}
       </Button>
       <p>
         Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
       </p>
     </div>
   );
