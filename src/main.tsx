7c15e664f695c06bf89bec0453416b88eb3aa994
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2024-10-17 13:56:19 +0800

    Change to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { ReactLenis } from "lenis/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "react-material-symbols/outlined";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import Layout from "./Layout.tsx";
 
@@ -11,11 +11,11 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ReactLenis root options={{ duration: 0.8 }}>
         <Header />
         <Layout />
       </ReactLenis>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
